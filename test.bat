54d1d2af776b1b76a9ad5a2a99cc64c1108b7e8d
Flux Flu <adamrlukies@gmail.com>
2024-01-02 10:52:56 -0500

    Removed batch file...

--- test.bat
+++
@@ -1,7 +0,0 @@
-node --trace-warnings .\src\fbc.js %*
-IF NOT %errorlevel%==0 (goto :eof)
-IF %errorlevel%==0 (
-    gcc -w -O3 ./temp.c -o temp.exe
-    IF NOT %errorlevel%==0 (goto :eof)
-    temp.exe
-)
